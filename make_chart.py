c36277c1451d526ad20d279709563271f20fb3b3
dpomondo <dpomondo@gmail.com>
2024-07-21 20:07:56 -0600

    added subplots

--- make_chart.py
+++ make_chart.py
@@ -21,15 +21,37 @@ parser.add_argument('filename',
                     help='(Optional) Name of file from which to make a chart')
 
 
+# partly stolen from:
+# https://stackoverflow.com/questions/68997190/typeerror-float-argument-must-be-a-string-or-a-number-not-datetime-time-in
 def make_chart(filename):
     with open(filename, 'r') as f:
         df = pd.read_csv(f, parse_dates=True, index_col='datetime')
-        df.plot(marker='+')
+        days = df.groupby(lambda ts: ts.dayofyear)
+        # df_hist = df.hist(column=" temp1")
 
-        plt.ylabel("Temp in C")
-        plt.xlabel("datetime")
-        plt.title(f"{filename}")
-        plt.grid(color='blue', linestyle='--')
+        # fig, axs = plt.subplots(2)
+        fig, axs = plt.subplot_mosaic("AA;BC")
+        # ax1 = fig.add_subplot()
+        for doy, grp in days:
+            axs["A"].plot(grp.index, grp, marker='+')
+            grp.index -= grp.index.floor('D')
+            axs["B"].plot(grp.index, grp)
+            axs["C"].hist(df, bins=20)
+
+        fig.suptitle(f"{filename}")
+        # plt.ylabel("Temp in C")
+        # plt.xlabel("datetime")
+        axs["A"].set_ylabel("Temp in C")
+        axs["A"].set_xlabel("datetime")
+        axs["B"].set_title("Day-on-Day Comparison")
+        axs["B"].set_ylabel("Temp in C")
+        axs["B"].set_xlabel("hour")
+        axs["C"].set_ylabel("Occurances")
+        axs["C"].set_xlabel("Temp")
+        # plt.title(f"{filename}")
+        axs["A"].grid(color='blue', linestyle='--')
+        axs["B"].grid(color='blue', linestyle='--')
+        axs["C"].grid(color='lightseagreen', linestyle='-')
         plt.show()
 
 
